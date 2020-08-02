15d4723be8c14ec70dcf4bc0ed317ffd7d4d50f0
Angela Romanovska <angelaromanovska@MacBook-Pro-Angela.local>
2020-08-02 15:58:03 +0200

    dodalam rozszerzony profil+mozliwosc edycji

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'avatar', 'bio')
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -11,7 +24,7 @@ class UserRegistrationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email')
 
     def clean_password2(self):
         cd = self.cleaned_data
--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    date_of_birth = models.DateField(blank=True, null=True)
+    avatar = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    bio = models.TextField()
+
+
+
+    def __str__(self):
+        return 'Profil użytkownika {}.'.format(self.user.username)
